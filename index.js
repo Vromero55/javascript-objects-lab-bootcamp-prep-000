b5c46d5653e78baf42014807e57ad3dd8895f0ea
Victor Romero <romeroa.victor@yahoo.com>
2020-05-29 18:56:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 function deleteFromObjectByKey(object, key){
   var clone = Object. assign({}, object); 
-  delete object[key];
+  delete clone[key];
   return clone;
 }//function updateObjectWithKeyAndValue
