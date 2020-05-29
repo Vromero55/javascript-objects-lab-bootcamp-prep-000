f662604c0176823bf0e029a568279fed41b6b106
Victor Romero <romeroa.victor@yahoo.com>
2020-05-29 18:38:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   return newobject;
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  
+  object[key] = value;
+  return object;
 }
 //function updateObjectWithKeyAndValue
